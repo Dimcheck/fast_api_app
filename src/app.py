4c458719e41214ea4e03f7475439d5654d15aacd
Dimcheck <dfarinyak@gmail.com>
2022-08-08 17:02:45 +0300

    added possibility to run fastapi from file.

--- src/app.py
+++ src/app.py
@@ -1,7 +1,8 @@
+import uvicorn
 from fastapi import FastAPI
 
-from routers import home, items
 from database.database import Base, engine
+from routers import home, items, token
 
 Base.metadata.create_all(bind=engine)
 
@@ -9,3 +10,7 @@ app = FastAPI()
 
 app.include_router(home.router)
 app.include_router(items.router)
+app.include_router(token.router)
+
+if __name__ == "__main__":
+    uvicorn.run('app:app', host='127.0.0.1', port=8000, reload=True)

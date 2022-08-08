1daabaa7c685e3b9abda544bb3a8193a4189a732
Dimcheck <dfarinyak@gmail.com>
2022-08-08 17:11:41 +0300

    added jwt authentication with status joke.

--- src/routers/home.py
+++ src/routers/home.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter, Request
+from auth.auth import oauth2_scheme
+from fastapi import APIRouter, Depends
 
 router = APIRouter(tags=["home"])
 
 
-@router.get('/')
-def home_page(request: Request):
-    return {'data': 'home page'}
+@router.get('/', dependencies=[Depends(oauth2_scheme)])
+def home_page():
+    return {'data': 'authenticated data'}
--- src/routers/items.py
+++ src/routers/items.py
@@ -1,20 +1,26 @@
-from fastapi import APIRouter, Depends
+from typing import List
+
+from auth.auth import oauth2_scheme
+from database.dependencies import get_db
+from fastapi import APIRouter, Depends, Response, status
+from fastapi.exceptions import HTTPException
 from models.items import Item
 from schemas.items import ItemSchema
 from sqlalchemy.orm import Session
-from database.dependencies import get_db
 from utils.crud import get_items, post_item
-
 
 router = APIRouter(tags=["items"])
 
 
-@router.get('/items/', response_model=list[ItemSchema])
+@router.get('/items/', dependencies=[Depends(oauth2_scheme)], response_model=List[ItemSchema])
 def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     items = get_items(db, skip=skip, limit=limit)
     return items
 
 
-@router.post('/items/', response_model=ItemSchema)
-def create_items(item: ItemSchema, db: Session = Depends(get_db)) -> Item:
+@router.post('/items/', dependencies=[Depends(oauth2_scheme)], response_model=ItemSchema)
+def create_items(resp: Response, item: ItemSchema, db: Session = Depends(get_db)) -> Item:
+    if item.is_broken is True:
+        resp.status_code = status.HTTP_418_IM_A_TEAPOT
+        raise HTTPException(detail='fix the item before posting it ;)', status_code=status.HTTP_418_IM_A_TEAPOT)
     return post_item(db=db, item=item)
